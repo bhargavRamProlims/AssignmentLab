e64f22b9fbdff6b17cb97d5e8b345c67fe741116
Bhargav <bhargav.ram@prolim.com>
2020-09-07 14:49:10 +0000

    Added new java files to test

---
+++ PracticeLab/src/practice_lab/LabPractice.java
@@ -0,0 +1,10 @@
+package practice_lab;
+
+public class LabPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Bhargav, this is a test file");
+	}
+
+}
